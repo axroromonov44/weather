cc4f35a05878c17fdeb0912449fb82c0ac3f631f
axroromonov <76530966+axroromonov44@users.noreply.github.com>
2025-08-20 14:57:18 +0500

    Initial Commit

---
+++ weather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weather
+//
+//  Created by IT-admin on 20/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by IT-admin on 20/08/25.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
